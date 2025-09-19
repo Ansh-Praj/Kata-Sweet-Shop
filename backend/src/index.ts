d5f8cc64f4c3b030224631670c96226ac76a25f9
Ansh Prajapat <anshprajapat8@gmail.com>
2025-09-19 20:36:11 +0530

    project init

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import Express from 'express'
+import router from './routes/index.js'
+
+
+const app = Express()
+const port = 3000
+
+app.use(Express.json())
+app.use(Express.urlencoded({extended: true}))
+app.use('/api', router)
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
