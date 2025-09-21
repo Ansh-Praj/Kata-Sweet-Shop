f9b5c951f9807751a62dcacf6504afa148945cf2
Ansh Prajapat <anshprajapat8@gmail.com>
2025-09-21 23:14:39 +0530

    remove extra console.log

--- backend/src/middleware/user.ts
+++ backend/src/middleware/user.ts
@@ -3,7 +3,6 @@ import jwt from "jsonwebtoken"
 
 export const userAuthenticate = ((req: Request, res: Response, next: NextFunction)=>{
     const token = req.headers.authorization?.split(" ")[1]
-    console.log("token", token);
     
     if(!token){
         return res.status(401).json({message: "Missing token"})
